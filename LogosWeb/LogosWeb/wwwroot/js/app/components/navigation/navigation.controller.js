a58fbc9841d53181fd2113fe701582177bf83fb0
Ricardo Cesar <musicricardinho@gmail.com>
2021-03-14 17:12:33 -0300

    arrumado bug no filtro dos cursos | adicionado link para palavra da direção

--- LogosWeb/LogosWeb/wwwroot/js/app/components/navigation/navigation.controller.js
+++ LogosWeb/LogosWeb/wwwroot/js/app/components/navigation/navigation.controller.js
@@ -6,46 +6,53 @@ function NavigationController($scope, $timeout) {
 
     var self = this;
 
-    self.baseUrl = window.location.protocol + '//' + window.location.host
+    self.baseUrl = window.location.protocol + '//' + window.location.host;
 
-    self.menuItems = [
-        { nome: 'Home', url: self.baseUrl + '/', subMenuItems: [] },
-        { nome: 'Sobre nós', url: self.baseUrl + '/Sobre', subMenuItems: [] },
-        {
-            nome: 'Cursos', url: self.baseUrl + '/Cursos', subMenuItems: [
-                {
-                    nome: 'Master', url: self.baseUrl + '/Cursos', subMenuItems: [
-                        { nome: 'Ministério', url: self.baseUrl + '/Cursos/Detalhes/7' },
-                        { nome: 'Aconselhamento Bíblico', url: self.baseUrl + '/Cursos/Detalhes/8' }
-                    ]
-                },
-                {
-                    nome: 'Pós-graduação', url: self.baseUrl + '/Cursos', subMenuItems: [
-                        { nome: 'Aconselhamento Bíblico', url: self.baseUrl + '/Cursos/Detalhes/3' },
-                        { nome: 'Exposição Bíblica', url: self.baseUrl + '/Cursos/Detalhes/4' },
-                        { nome: 'Liderança Cristã', url: self.baseUrl + '/Cursos/Detalhes/5' },
-                        { nome: 'Educação Musical', url: self.baseUrl + '/Cursos/Detalhes/6' },
-                    ]
-                },
-                {
-                    nome: 'Graduação', url: self.baseUrl + '/Cursos', subMenuItems: [
-                        { nome: 'Teologia Ministerial', url: self.baseUrl + '/Cursos/Detalhes/1', subMenuItems: [] }
-                    ]
-                },
-                { nome: 'Música Sacra', url: self.baseUrl + '/Cursos/Detalhes/2', subMenuItems: [] },
-            ]
-        },
-        { nome: 'Professores', url: self.baseUrl + '/Professores', subMenuItems: [] },
-        { nome: 'Blog', url: self.baseUrl + '/Blog', subMenuItems: []},
-        { nome: 'Fale conosco', url: self.baseUrl + '/Contato', subMenuItems: [] },
-        { nome: 'Área do aluno', url: 'https://portal.sponteeducacional.net.br/default.aspx?CID=71311', subMenuItems: [] }
-    ];
+    self.menuItems = [];
 
     self.init = function () {
+
+        self.setCursosMenuItem();
 
         $timeout(function () {
             self.startPortfolio(); //método do custom.min.js
         }, 500)
+    }
+
+    self.setCursosMenuItem = function () {
+
+        self.menuItems = [
+            { nome: 'Home', url: self.baseUrl + '/', subMenuItems: [] },
+            { nome: 'Sobre nós', url: self.baseUrl + '/Sobre', subMenuItems: [] },
+            {
+                nome: 'Cursos', url: self.baseUrl + '/Cursos', subMenuItems: [
+                    {
+                        nome: 'Master', url: self.baseUrl + '/Cursos', subMenuItems: [
+                            { nome: 'Ministério', url: self.baseUrl + '/Cursos/Detalhes/7' },
+                            { nome: 'Aconselhamento Bíblico', url: self.baseUrl + '/Cursos/Detalhes/8' }
+                        ]
+                    },
+                    {
+                        nome: 'Pós-graduação', url: self.baseUrl + '/Cursos', subMenuItems: [
+                            { nome: 'Aconselhamento Bíblico', url: self.baseUrl + '/Cursos/Detalhes/3' },
+                            { nome: 'Exposição Bíblica', url: self.baseUrl + '/Cursos/Detalhes/4' },
+                            { nome: 'Liderança Cristã', url: self.baseUrl + '/Cursos/Detalhes/5' },
+                            { nome: 'Educação Musical', url: self.baseUrl + '/Cursos/Detalhes/6' },
+                        ]
+                    },
+                    {
+                        nome: 'Graduação', url: self.baseUrl + '/Cursos', subMenuItems: [
+                            { nome: 'Teologia Ministerial', url: self.baseUrl + '/Cursos/Detalhes/1', subMenuItems: [] }
+                        ]
+                    },
+                    { nome: 'Música Sacra', url: self.baseUrl + '/Cursos/Detalhes/2', subMenuItems: [] },
+                ]
+            },
+            { nome: 'Professores', url: self.baseUrl + '/Professores', subMenuItems: [] },
+            { nome: 'Blog', url: self.baseUrl + '/Blog', subMenuItems: [] },
+            { nome: 'Fale conosco', url: self.baseUrl + '/Contato', subMenuItems: [] },
+            { nome: 'Área do aluno', url: 'https://portal.sponteeducacional.net.br/default.aspx?CID=71311', subMenuItems: [] }
+        ];
     }
 
     self.startPortfolio = function () {
@@ -66,18 +73,29 @@ function NavigationController($scope, $timeout) {
             return false;
         });
 
-        $(window).on('resize', function () {
-            worksgrid.imagesLoaded(function () {
-                worksgrid.isotope({
-                    layoutMode: 'masonry',
-                    itemSelector: '.portfolio-item',
-                    transitionDuration: '0.4s',
-                    masonry: {
-                        columnWidth: '.grid-sizer',
-                    },
-                });
+        worksgrid.imagesLoaded(function () {
+            worksgrid.isotope({
+                layoutMode: 'masonry',
+                itemSelector: '.portfolio-item',
+                transitionDuration: '0.4s',
+                masonry: {
+                    columnWidth: '.grid-sizer',
+                },
             });
         });
+
+        //$(window).on('resize', function () {
+        //    worksgrid.imagesLoaded(function () {
+        //        worksgrid.isotope({
+        //            layoutMode: 'masonry',
+        //            itemSelector: '.portfolio-item',
+        //            transitionDuration: '0.4s',
+        //            masonry: {
+        //                columnWidth: '.grid-sizer',
+        //            },
+        //        });
+        //    });
+        //});
     }
 
 }

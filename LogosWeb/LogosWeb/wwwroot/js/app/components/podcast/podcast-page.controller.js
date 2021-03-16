62d1cdf42203f5763c1053ff2f9596fdbe44aa65
Ricardo Cesar <musicricardinho@gmail.com>
2021-03-16 00:42:28 -0300

    adicionado angular controller para pagina de podcast

---
+++ LogosWeb/LogosWeb/wwwroot/js/app/components/podcast/podcast-page.controller.js
@@ -0,0 +1,70 @@
+﻿angular
+    .module('logosApp')
+    .controller('PodcastPageController', ['$scope', '$timeout', PodcastPageController]);
+
+function PodcastPageController($scope, $timeout) {
+
+    var self = this;
+
+    self.content = [];
+
+    self.podcastContent = {};
+
+    self.init = function () {
+
+        self.content = [
+            { nome: 'Devocional', descricao: 'bla bla bla' },
+            { nome: 'Estudo', descricao: 'bla bla bla' },
+            { nome: 'Teologia', descricao: 'bla bla bla' }
+        ];
+
+        $timeout(function () {
+            self.refreshCarousel();
+        }, 500);
+    }
+
+    self.showPodcastList = function (podcast) {
+
+        self.podcastContent = podcast;
+
+        $('#podcast-list-section').show();
+
+        self.scrollToList();
+    }
+
+    self.scrollToList = function () {
+
+        $('html, body').animate({
+            scrollTop: $('##podcast-list-section').offset().top - 100
+        }, 1000);
+    }
+
+    self.refreshCarousel = function () {
+
+        if ($('.review-carousel').length > 0) {
+
+            $('.review-carousel').trigger('destroy.owl.carousel');
+
+            $('.review-carousel').owlCarousel($.extend({
+                nav: 0,
+                dots: 1,
+                autoplay: 1,
+                items: 1,
+                loop: 1,
+                margin: 30,
+                responsive: {
+                    768: {
+                        items: 2
+                    },
+                    1025: {
+                        items: 3
+                    }
+                },
+                navText: [
+                    '<span class="ti-angle-left"></span>',
+                    '<span class="ti-angle-right"></span>'
+                ],
+            }, $('.review-carousel').data('carousel-options')));
+        }
+    }
+}
